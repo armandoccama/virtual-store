733606bc104507fa60b3de43ecf0891516b121da
armandoccama <ccapo4matemacc@gmail.com>
2024-06-18 20:46:44 -0500

    implementacion de redux-toolkits

--- src/interfaces/hook.ts
+++ src/interfaces/hook.ts
@@ -2,6 +2,5 @@
 import { TypedUseSelectorHook, useDispatch as useReduxDispatch, useSelector as useReduxSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../store/index';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useDispatch: () => AppDispatch = useReduxDispatch;
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
